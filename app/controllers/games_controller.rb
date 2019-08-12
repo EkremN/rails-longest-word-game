baea19ad653c79476e1e2df908edfbb870826fd6
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-12 16:20:32 +0100

    Validating words

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,19 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   def new
     @letters = (0...8).map { ('a'..'z').to_a[rand(26)] }
   end
 
   def score
-    # raise
     @score = params[:answer]
+    @score_arr = params[:answer].chars
+    @letters = params[:letters].split()
+    @valid = @score_arr.all? { |letter| @score_arr.count(letter) <= @letters.count(letter) }
+
+    result = open("https://wagon-dictionary.herokuapp.com/#{params[:answer]}").read
+    @english_word = JSON.parse(result)["found"]
+    # raise
   end
 end
+
